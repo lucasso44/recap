196799bc2526a3daad8a8dbdbfe70dd5ff6d869a
Lucas Lawes <k1736700@kingston.ac.uk>
2019-10-29 20:48:23 +0000

    Add version 2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello from recap.cloud!</h1>');
+    res.send('<h1>Hello from recap.cloud v2!</h1>');
 });
 
 app.listen(3000, () => {
